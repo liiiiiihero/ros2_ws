9b36a0d7735c4553cf8b1738e8d44b00df526315
li <1572923618@qq.com>
2026-06-24 13:37:01 +0800

    2026.6.24 1.36

---
+++ demo_cpp/src/learn_function.cpp
@@ -0,0 +1,59 @@
+#include "iostream"
+#include "functional"
+
+//自由函数
+
+void save_with_free_fun( const std::string & file_name)
+{
+    std::cout<<"自由函数:"<<file_name<<std::endl;
+}
+
+
+//成员函数
+
+class File_save
+{
+private:
+    /* data */
+public:
+    File_save(/* args */)=default;
+    ~File_save()=default;
+
+
+    void save_with_member_fun( const std::string & file_name)
+    {
+      std::cout<<"成员函数:"<<file_name<<std::endl;
+    };
+
+};
+
+int main()
+{
+    File_save file_save;
+
+    //lambda
+    auto save_with_lambda_fun = [](const std::string &file_name)->void
+    {
+        std::cout<<"lambda函数:"<<file_name<<std::endl;
+
+
+    };
+    //调用
+    //save_with_free_fun("xiaoliii");
+    //file_save.save_with_member_fun("xiaoliii");
+    //save_with_lambda_fun("xiaoliii");
+
+    //包装器
+    std::function<void(const std:: string&)>save1=save_with_free_fun;
+    std::function<void(const std:: string&)>save2=save_with_lambda_fun;
+    //成员函数放入包装器,不能直接扶植，最后为占位符，因一个成员函数故为1
+    std::function<void(const std:: string&)>save3=std::bind(&File_save::save_with_member_fun,&file_save,std::placeholders::_1);
+    //包装后调用
+    save1("hahahah1");
+    save2("hahahah2");
+    save3("hahahah3");
+
+
+
+    return 0;
+}
